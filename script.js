18030464341707fb44842d243b7702ee018bf409
aoyetibo557 <aoyetibo@gmail.com>
2020-12-18 13:02:22 -0500

    Updated projects section 12/18/20

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-window.addEventListener('scroll', function(){
+jQuery(document).ready(function($) {
+    $('#autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cs-hidden');
+        }
+    });
+
+});
+
+window.addEventListener('scroll', function() {
     var header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
 
-function toggleMenu(){
+
+function toggleMenu() {
     var menuToggle = document.querySelector('.toggle');
     var menu = document.querySelector('.menu');
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active');
 
 }
-function showHiddenProject(){
+
+function showHiddenProject() {
     var projDiv = document.getElementById("hidden__projects");
-    projDiv.classList.remove("_hidden__projects"); 
+    projDiv.classList.remove("_hidden__projects");
 
     // Add the content class to the div 
 
@@ -22,7 +35,7 @@ function showHiddenProject(){
     // Replace View More with Show less
     document.getElementById("_viewMore_btn").innerHTML = "Show Less";
 
-    if(projDiv.addEventListener("click")){
-        projDiv.classList.add("_hidden__projects"); 
+    if (projDiv.addEventListener("click")) {
+        projDiv.classList.add("_hidden__projects");
     }
 }
